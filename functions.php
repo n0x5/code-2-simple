40f9c00fa1a4a205d6596d733d82ceafa027d9e7
n0x5 <n0x5@users.noreply.github.com>
2021-08-09 18:23:18 +0200

    fixes

--- functions.php
+++ functions.php
@@ -42,5 +42,24 @@ wp_enqueue_style( 'data-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'data_scripts' );
 
+function gretathemes_meta_description() {
+    global $post;
+    if ( is_singular() ) {
+        //$des_post = strip_tags( $post->post_content );
+        //$des_post = strip_shortcodes( $post->post_content );
+        //$des_post = str_replace( array("\n", "\r", "\t"), ' ', $des_post );
+        //$des_post = mb_substr( $des_post, 0, 300, 'utf8' );
+        //echo '<meta name="description" content="' . $des_post . '" />' . "\n";
+        echo '<meta name="description" content="' . get_bloginfo( "description" ) . '" />' . "\n";
+    }
+    if ( is_home() ) {
+        echo '<meta name="description" content="' . get_bloginfo( "description" ) . '" />' . "\n";
+    }
+    if ( is_category() ) {
+        $des_cat = strip_tags(category_description());
+        echo '<meta name="description" content="' . $des_cat . '" />' . "\n";
+    }
+}
+add_action( 'wp_head', 'gretathemes_meta_description');
 
 ?>
--- index.php
+++ index.php
@@ -6,15 +6,17 @@
 <body <?php body_class(); ?>>
 <section class="content">
 	
-	
+
 <?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); ?>
-
+	
+<div class="entry">
 <h2> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <?php the_time('F jS, Y') ?></h2>
-
 <?php the_content('-> read more'); ?>
-
+</div>
+	
 <?php endwhile; ?>
 <?php endif; ?>
+
 </section>
 
 <?php get_sidebar(); ?>
