cb93c13bfbe3b353c4272d390868c11bdd0111d8
n0x5 <n0x5@users.noreply.github.com>
2021-08-11 15:25:11 +0200

    nothowitworks

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" wrapper="width=device-width, initial-scale=1">    
+<meta charset="<?php bloginfo( 'charset' ); ?>">    
 <link rel="icon" type="image/x-ico" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" />    
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
